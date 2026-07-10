288838a893ef3ed8b6d949278f11541549ed7739
Keno <keno.mandrysch@gmail.com>
2026-07-10 15:44:08 +0200

    Step 8: DEBUG is read from an environment variable, defaulting to False (#2)

--- tests/test_settings_security.py
+++ tests/test_settings_security.py
@@ -8,3 +8,7 @@ def _settings_source():
 
 def test_secret_key_is_read_from_env():
     assert "SECRET_KEY = env(" in _settings_source()
+
+
+def test_debug_is_read_from_env():
+    assert "DEBUG = env.bool(" in _settings_source()
